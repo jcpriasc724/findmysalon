cd0000ab914f067665132cacd02b756066c0d135
jcpriasc724 <camilo479@gmail.com>
2020-08-11 23:20:52 +1000

    Fix Business
    Fix Business

--- FindMySalon/app/src/main/java/com/findmysalon/view/adapters/BusinessAdapter.java
+++ FindMySalon/app/src/main/java/com/findmysalon/view/adapters/BusinessAdapter.java
@@ -39,7 +39,7 @@ public class BusinessAdapter extends RecyclerView.Adapter<BusinessAdapter.Busine
         holder.txtNameBusiness.setText(list.get(position).getStoreName());
         holder.txtAddress.setText(list.get(position).getAddress());
         holder.txtPhoneNumber.setText(list.get(position).getPhone());
-        //holder.rtbBusiness.setRating(list.get(position).get());
+        holder.rtbBusiness.setRating(list.get(position).getRating());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
