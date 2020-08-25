ada8899f111de222e529b35a5549353ecc51ba82
jcpriasc724 <camilo479@gmail.com>
2020-08-25 17:04:58 +1000

    fix login
    fix login

--- FindMySalon/app/src/main/java/com/findmysalon/view/LoginActivity.java
+++ FindMySalon/app/src/main/java/com/findmysalon/view/LoginActivity.java
@@ -62,11 +62,11 @@ public class LoginActivity extends AppCompatActivity {
 
         btnSignin.setOnClickListener(v -> {
 
-            Intent intent = new Intent(LoginActivity.this, CustomerActivity.class);
-            //ide .putExtra("hi", "HI");
-            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-//            signIn();
+//            Intent intent = new Intent(LoginActivity.this, CustomerActivity.class);
+//            //ide .putExtra("hi", "HI");
+//            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(intent);
+            signIn();
         });
 
         btnSignup.setOnClickListener(v -> {
